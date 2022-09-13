5e58d0b8afce2d33fde7d138809a6a748d0847f2
Michael Morales <mt.morales@hotmail.fr>
2022-09-13 12:14:01 +0200

    basic mobile dashboard almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardsModule } from './boards/boards.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BoardsModule],
+  imports: [BrowserModule, AppRoutingModule, BoardsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
