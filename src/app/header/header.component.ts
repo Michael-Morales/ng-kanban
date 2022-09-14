5233cae82df6d4020b7935eb0219f63f39740151
Michael Morales <mt.morales@hotmail.fr>
2022-09-14 15:15:04 +0200

    started working on modal commponent

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,7 @@ import { Board } from '../interfaces';
 export class HeaderComponent implements OnInit {
   boards?: Board[];
   currentBoard?: Board;
+  showModal = false;
 
   constructor(private router: Router, private boardsService: BoardsService) {}
 
@@ -33,5 +34,13 @@ export class HeaderComponent implements OnInit {
       .subscribe((board) => {
         this.currentBoard = board;
       });
+  }
+
+  onOpenModal() {
+    this.showModal = true;
+  }
+
+  onCloseModal() {
+    this.showModal = false;
   }
 }
