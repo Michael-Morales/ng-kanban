6af144ce37fc3645bd5d3d4afaf650ef016a68c7
Michael Morales <mt.morales@hotmail.fr>
2022-09-15 16:46:26 +0200

    added nav menu to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,7 @@ import { Board } from '../interfaces';
 export class HeaderComponent implements OnInit {
   boards?: Board[];
   currentBoard?: Board;
+  showNav = false;
 
   constructor(private router: Router, private boardsService: BoardsService) {}
 
@@ -32,6 +33,7 @@ export class HeaderComponent implements OnInit {
       )
       .subscribe((board) => {
         this.currentBoard = board;
+        this.showNav = false;
       });
   }
 }
