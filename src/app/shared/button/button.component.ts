8b64290f916979935b977937484d45227f06f6f8
Michael Morales <mt.morales@hotmail.fr>
2022-10-05 16:28:25 +0200

    started working on responsiveness of header

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() buttonType = 'button';
   @Input() buttonStyle = [''];
+  @Input() disabled = false;
 
   constructor() {}
 
