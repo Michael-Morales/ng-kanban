1c1d685d5f325c1abb89f9c73c7200cd15531a54
mmorales <mt.morales@hotmail.fr>
2022-10-13 10:35:52 +0200

    added tasks reducer

--- src/app/boards/state/tasks.reducer.ts
+++ src/app/boards/state/tasks.reducer.ts
@@ -2,6 +2,16 @@ import { createReducer, on } from '@ngrx/store';
 
 import { Task } from '../../interfaces';
 
+import { fetchTasks, addTask, deleteTask, updateTask } from './tasks.actions';
+
 export const initialState: ReadonlyArray<Task> = [];
 
-export const tasksReducer = createReducer(initialState);
+export const tasksReducer = createReducer(
+  initialState,
+  on(fetchTasks, (_, { tasks }) => tasks),
+  on(addTask, (state, { task }) => [...state, task]),
+  on(deleteTask, (state, { id }) => state.filter((task) => task.id !== id)),
+  on(updateTask, (state, { id, data }) =>
+    state.map((task) => (task.id === id ? { ...task, ...data } : task))
+  )
+);
