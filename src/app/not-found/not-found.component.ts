34238c58b118f0c3311ee5e262f07981ee2a3dee
mmorales <mt.morales@hotmail.fr>
2022-10-19 11:48:22 +0200

    added dismiss header menu to not found component

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeaderService } from '../header/header.service';
+
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent implements OnInit {
+  constructor(private headerService: HeaderService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  dismissHeaderMenu() {
+    if (
+      this.headerService.showMenu ||
+      this.headerService.showNav ||
+      this.headerService.showSidebar
+    ) {
+      this.headerService.closeMenus();
+    }
   }
-
 }
